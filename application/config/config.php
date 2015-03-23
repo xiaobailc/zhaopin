76aaa26e8b5d93adb677ad1d567eb7d80a97b7d8
刘畅 <lc918.student@sina.com>
2015-03-23 15:05:30 +0800

    update config
    update config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/zhaopin/';
+$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/zhaopin/';
 
 /*
 |--------------------------------------------------------------------------
